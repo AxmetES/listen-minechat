f400a2cd0dc3444b41f10125bfaa1a6ff10c1678
axmetes <fraktsia@gmail.com>
2023-11-17 03:00:27 +0600

    Lesson 4, Step: 8. Add logging.

--- listen-minechat.py
+++ listen-minechat.py
@@ -1,42 +1,60 @@
 import datetime
 import asyncio
+import logging
+
 import aiofiles
 import configargparse
+
+
+logging.basicConfig(
+    filename="write.log",
+    level=logging.DEBUG,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
 
 
 parser = configargparse.ArgumentParser(
     description="host, port, file path",
     default_config_files=["myconfig.ini"],
-    add_help=False
+    add_help=False,
 )
 
-parser.add_argument("--help", "-help", action="help", help="Show this help message and exit")
-parser.add_argument('--host', '-h', dest='host', help="server address")
-parser.add_argument('--port', '-p', dest='port', help="servers port")
-parser.add_argument("--path", '-pt', dest='path', help="Path to input file")
+parser.add_argument(
+    "--help", "-help", action="help", help="Show this help message and exit"
+)
+parser.add_argument("--host", "-h", dest="host", help="server address")
+parser.add_argument("--port", "-p", dest="port", help="servers port")
+parser.add_argument("--path", "-pt", dest="path", help="Path to input file")
 
 args = parser.parse_args()
 
 
 async def append_to_file(data):
     filename = args.path
-    async with aiofiles.open(filename, mode='a') as file:
-        now = datetime.datetime.now()
-        date = now.strftime('%d.%m.%y %H:%M')
-        print(data)
-        if data != '\n' and data != '':
-            await file.write(f'[{date}] {data}\n')
+    try:
+        async with aiofiles.open(filename, mode="a") as file:
+            now = datetime.datetime.now()
+            date = now.strftime("%d.%m.%y %H:%M")
+            if data != "\n" and data != "":
+                await file.write(f"[{date}] {data}\n")
+    except OSError as e:
+        logging.error(f"Error writing to file: {e}")
 
 
 async def tcp_echo_client():
-    reader, writer = await asyncio.open_connection(
-        args.host, args.port)
+    try:
+        reader, writer = await asyncio.open_connection(args.host, args.port)
+    except OSError as e:
+        logging.error(f"Error connecting to the server: {e}")
+        return
     data = await reader.read(1000)
     while data:
         data = await reader.read(1000)
+        logging.info(f"Received: {data}")
         await append_to_file(data.decode().strip())
 
-    print(f'Received: {data.decode()!r}')
+    logging.info(f"Received: {data.decode()!r}")
+    print(f"Received: {data.decode()!r}")
 
 
 asyncio.run(tcp_echo_client())
--- write-minechat.py
+++ write-minechat.py
@@ -1,54 +1,91 @@
+import logging
 import asyncio
 import json
 
 import redis.asyncio as redis
 
-from config import settings
+
+logging.basicConfig(
+    filename="write.log",
+    level=logging.DEBUG,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
 
 
 async def get_hash(response: str):
-    body = json.loads(response.split("\n")[0])
-    return body["account_hash"]
+    body = None
+    try:
+        body = json.loads(response.split("\n")[0])
+    except json.JSONDecodeError as json_error:
+        logging.error(f"JSON decoding error: {json_error}")
+    account_hash = body["account_hash"]
+    nickname = body["nickname"]
+    return nickname, account_hash
 
 
 async def send_messages(reader, writer):
     value = None
     client = redis.Redis()
     while True:
-        response = await reader.read(500)
-        print(f"from server: {response.decode()}")
+        try:
+            response = await reader.readline(500)
+        except asyncio.IncompleteReadError as incomplete_read_error:
+            logging.error(f"Incomplete read error: {incomplete_read_error}")
+            break
+        except asyncio.CancelledError:
+            logging.info("Task cancelled.")
+            break
+        except ConnectionError as connection_error:
+            logging.error(f"Connection error: {connection_error}")
+            break
+        except OSError as os_error:
+            logging.error(f"OS error: {os_error}")
+            break
+        logging.info(f"from server: {response.decode()}")
         if (
             response.decode()
             == "Hello %username%! Enter your personal hash or leave it empty to create new account.\n"
         ):
             value = await client.get("account_hash")
             if value:
+                logging.info(f"to server: {response.decode()}")
                 writer.write(value + b"\n")
                 await writer.drain()
             else:
+                logging.info(f"to server: {response.decode()}")
                 writer.write(" ".encode("utf-8") + b"\n")
                 await writer.drain()
         if response.decode() == "Enter preferred nickname below:\n":
             message = input("Enter preferred nickname: ")
+            logging.info(f"ser info: {response.decode()}")
             writer.write(message.encode("utf-8") + b"\n" + b"\n")
             await writer.drain()
         if "account_hash" in response.decode():
-            account_hash = await get_hash(response.decode())
+            nickname, account_hash = await get_hash(response.decode())
+            logging.info(f"nickname: {response.decode()}")
             if not value:
-                await client.set("account_hash", account_hash)
-            break
+                logging.info("account_hash saved.")
+                try:
+                    await client.set("account_hash", account_hash)
+                except redis.RedisError as redis_error:
+                    logging.error(f"Redis error: {redis_error}")
+                    break
     while True:
         if not response:
-            print("Server did not respond within the timeout. Exiting.")
+            logging.info("Server did not respond within the timeout. Exiting.")
             break
         else:
             message = input("Enter your message: ")
+            logging.info(f"message to server: {message}")
             writer.write(message.encode("utf-8") + b"\n" + b"\n")
             await writer.drain()
 
 
 async def main():
-    reader, writer = await asyncio.open_connection("minechat.dvmn.org", 5050)
+    host = "minechat.dvmn.org"
+    port = 5050
+    reader, writer = await asyncio.open_connection(host=host, port=port)
+    logging.info(f"host: {host}, port: {port}")
     send_task = asyncio.create_task(send_messages(reader, writer))
     await asyncio.gather(send_task)
 
